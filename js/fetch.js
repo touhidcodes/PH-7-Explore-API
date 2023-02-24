56d9208d6696e303b3934295446fbabd322010b4
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-25 00:01:12 +0600

    fetch

---
+++ js/fetch.js
@@ -0,0 +1,11 @@
+/* fetch("https://jsonplaceholder.typicode.com/todos/1")
+	.then((response) => response.json())
+	.then((json) => console.log(json));
+ */
+
+function loadData() {
+    const url = "https://jsonplaceholder.typicode.com/todos/1";
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+}
